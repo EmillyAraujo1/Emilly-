5a9aee98eb1b6ffe3f23b2e15fa1957be283b991
EMILLY ARAUJO DE CARVALHO <50975251830@imaccommand042.local>
2025-11-18 10:46:07 -0300

    ëcxe

---
+++ exemplo.js
@@ -0,0 +1,42 @@
+//window.alert("Ola")
+
+//window.confirm("O Jonas vai casar!")
+
+//window.prompt("Qual o seu nome?")
+
+//console.log("Multimidia")
+
+//let nome =prompt("Informe o seu nome: ")
+
+//console.log("Bem vindo!" + nome)
+
+//let numero1 = Number.parseInt(prompt("Digite o primeiro número: ") )
+//let numero2 = Number.parseInt(prompt("Digite o segundo número"))
+
+//let numero1 = Number.parseFloat(prompt("Digite o primeiro número: ") )
+//let numero2 = Number.parseFloat(prompt("Digite o segundo número"))
+
+//let resultado = numero1 + numero2
+
+
+//console.log(resultado)
+if (idade >= 18) {
+
+    let idade = window.prompt("Maior de idade!Daleee")
+} else {
+    console.log("Você é menor de idade!")
+}
+
+switch (idade) {
+    case "18":
+        console.log("Maior de idade!DALEE")
+        break;
+
+    case "27":
+        console.log("Está perto do 30")
+        break;
+
+    default:
+        console.log("Nada a dizer")
+        break;
+}
